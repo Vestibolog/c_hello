e77c121b29d39ba62c631d8e7647743522d0a00f
Vestibolog <tarn@ngs.ru>
2022-04-30 21:57:45 +0700

    add exemple 5 сделали имена

---
+++ example_05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine( "Введите имя пользователя" );
+string userName = Console.ReadLine();
+
+if ( userName.ToLower() == "рома" )
+{
+    Console.WriteLine( "Ура это же Рома !!!" );
+} else
+{
+    Console.Write( "Привет,  " );
+    Console.WriteLine( userName );
+}
